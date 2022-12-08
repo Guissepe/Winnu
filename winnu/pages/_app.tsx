edafe0d7d906674f6665cec80d1fae9773f54d2d
Jesutooni <edeworuyoyou@gmail.com>
2022-12-08 09:22:40 +0100

    setup chakra ui

--- winnu/pages/_app.tsx
+++ winnu/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  brand: {
+    primary: "#FCCF47",
+    secondary: "#FFAFA3",
+    700: "#2a69ac",
+  },
+};
+
+const theme = extendTheme({ colors });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }

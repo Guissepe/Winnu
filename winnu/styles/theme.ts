70f2230d0deb9793ba1587e69db3a99385182e05
Jesutooni <edeworuyoyou@gmail.com>
2022-12-13 12:19:20 +0100

    add custom fonts

--- winnu/styles/theme.ts
+++ winnu/styles/theme.ts
@@ -1,5 +1,7 @@
 import { extendTheme } from "@chakra-ui/react";
 import { buttonStyles as Button } from "./components/buttonStyles";
+import "@fontsource/manrope";
+import "@fontsource/sora";
 
 export const theme = extendTheme({
   colors: {
@@ -7,6 +9,21 @@ export const theme = extendTheme({
       primary: "#FCCF47",
       secondary: "#FFAFA3",
     },
+    text: {
+      grey02: "#383838",
+      peach: "#E1B0A8",
+    },
+  },
+  fonts: {
+    sora: "Sora, sans-serif",
+    manrope: "Manrope, sans-serif",
+  },
+  breakpoints: {
+    sm: "520px",
+    md: "768px",
+    lg: "960px",
+    xl: "1200px",
+    "2xl": "1536px",
   },
   components: {
     Button,

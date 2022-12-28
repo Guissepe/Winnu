8ea2c7c667e6a1798bd6e71f0d24dba7eeee0213
natha ngabriel <nathangabrielvf@gmail.com>
2022-12-28 10:42:58 -0300

    added callbackurl

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -3,10 +3,6 @@ import {
     Box,
     Button,
     Flex,
-    Grid,
-    GridItem,
-    Show,
-    Text,
     Image,
   } from "@chakra-ui/react";
 
@@ -18,7 +14,7 @@ export default function SignIn({ providers }: { providers: any }) {
       {Object.values(providers).map((provider: any) => (
         <Flex key={provider.name}>
           <Image src="https://raw.githubusercontent.com/nextauthjs/next-auth/main/packages/next-auth/provider-logos/github.svg"></Image>
-          <Button fontFamily="Sora" variant="primary" py="16px" px="35px" borderRadius="3px" onClick={() => signIn(provider.id, { callbackUrl: 'https://winnu-cht0kn623-guissepe.vercel.app' })}>
+          <Button fontFamily="Sora" variant="primary" py="16px" px="35px" borderRadius="3px" onClick={() => signIn(provider.id, { callbackUrl: 'https://winnu-git-auth-guissepe.vercel.app' })}>
             Sign in with {provider.name}
           </Button>
         </Flex>

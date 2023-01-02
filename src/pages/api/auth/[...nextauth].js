280b02a4c1733fc299f98c46d946a8ab3efe1986
natha ngabriel <nathangabrielvf@gmail.com>
2023-01-02 17:49:45 -0300

    Fix mislable

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,20 +1,20 @@
 import NextAuth from 'next-auth';
-import { Provider } from 'next-auth/providers';
+import { Providers } from 'next-auth/providers';
 
 const options = {
     debug: true,
     providers: [
-        Provider.Facebook({
+        Providers.Facebook({
             clientId: process.env.FACEBOOK_CLIENT_ID,
             clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
         }),
 
-        Provider.Google({
+        Providers.Google({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
 
-        Provider.GitHub({
+        Providers.GitHub({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
         }),

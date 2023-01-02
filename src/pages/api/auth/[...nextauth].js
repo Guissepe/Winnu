cd9bf28c5b777c6c7b5af83a714cb993849fec3d
natha ngabriel <nathangabrielvf@gmail.com>
2023-01-02 17:47:51 -0300

    Testing solutions

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,22 +1,20 @@
 import NextAuth from 'next-auth';
-import GitHubProvider from 'next-auth/providers/github';
-import GoogleProvider from 'next-auth/providers/google';
-import FacebookProvider from 'next-auth/providers/facebook';
+import { Provider } from 'next-auth/providers';
 
 const options = {
     debug: true,
     providers: [
-        FacebookProvider({
+        Provider.Facebook({
             clientId: process.env.FACEBOOK_CLIENT_ID,
             clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
         }),
 
-        GoogleProvider({
+        Provider.Google({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
 
-        GitHubProvider({
+        Provider.GitHub({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
         }),

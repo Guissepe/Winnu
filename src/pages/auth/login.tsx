1ac0b926feabe9bda58b59029d08334ce9c778d7
natha ngabriel <nathangabrielvf@gmail.com>
2023-01-02 17:58:14 -0300

    changed onclick

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -12,7 +12,7 @@ export default function SignIn({ providers }: { providers: any }) {
     <Box background='black'>
       {Object.values(providers).map((provider: any) => (
          <Flex key={provider.name}>
-          <Button fontFamily="Sora" variant="primary" py="16px" px="35px" borderRadius="3px" onClick={() => signIn(provider.id, { callbackUrl: 'https://winnu-git-fix-guissepe.vercel.app' })}>
+          <Button fontFamily="Sora" variant="primary" py="16px" px="35px" borderRadius="3px" onClick={() => signIn()}>
             Sign in with {provider.name}
           </Button>
         </Flex>

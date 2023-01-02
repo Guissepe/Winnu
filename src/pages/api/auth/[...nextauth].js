fb471d461e6675b7da8ecfa3b9fcf85ca30e8652
natha ngabriel <nathangabrielvf@gmail.com>
2023-01-02 17:51:59 -0300

    Changed provider imports

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,20 +1,23 @@
 import NextAuth from 'next-auth';
-import { Providers } from 'next-auth/providers';
+import GitHubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import FacebookProvider from 'next-auth/providers/facebook';
+import { Provider } from 'next-auth/providers';
 
 const options = {
     debug: true,
     providers: [
-        Providers.Facebook({
+        FacebookProvider({
             clientId: process.env.FACEBOOK_CLIENT_ID,
             clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
         }),
 
-        Providers.Google({
+        GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
 
-        Providers.GitHub({
+        GitHubProvider({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
         }),

0a24e880d4782d4f1285a337ad67a8d50d85ad5f
natha ngabriel <nathangabrielvf@gmail.com>
2023-01-02 15:50:57 -0300

    Testing solutions

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { getProviders, signIn } from "next-auth/react"
 import {
     Box,
@@ -7,34 +6,31 @@ import {
     Image,
   } from "@chakra-ui/react";
 
-
 export default function SignIn({ providers }: { providers: any }) {
-  return (
-    <>
-    <Box background='black'>
-      {Object.values(providers).map((provider: any) => (
-         <Flex key={provider.name}>
-          <Image src="https://raw.githubusercontent.com/nextauthjs/next-auth/main/packages/next-auth/provider-logos/github.svg"></Image>
-          <Button fontFamily="Sora" variant="primary" py="16px" px="35px" borderRadius="3px">
-            Sign in with
-          </Button>
-        </Flex>
-          ))}
-    </Box>
-
-    </>
-  )
-}
-
-export async function getServerSideProps(context: any) {
     try {
         throw new Error('Something bad happened');
       }
       catch(e) {
         console.log(e);
-      }
+      };
+  return (
+    <>
+    <Box background='black'>
+      {Object.values(providers).map((provider: any) => (
+         <Flex key={provider.name}>
+          <Button fontFamily="Sora" variant="primary" py="16px" px="35px" borderRadius="3px">
+            Sign in with {provider.name}
+          </Button>
+        </Flex>
+          ))}
+    </Box>
+    </>
+  );
+};
+
+export async function getServerSideProps(context: any) {
   const providers = await getProviders()
   return {
     props: { providers },
-  }
-}
+  };
+};

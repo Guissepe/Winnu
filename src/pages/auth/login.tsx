6dee9a7b3b6d34f59f4a79a6e0ddfcfbf850610a
natha ngabriel <nathangabrielvf@gmail.com>
2023-01-02 13:05:01 -0300

    Reset code to working version

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,60 +1,28 @@
-import { getProviders, signIn, getSession, getCsrfToken } from "next-auth/react"
+import { getProviders, signIn } from "next-auth/react"
 import {
     Box,
     Button,
     Flex,
     Image,
   } from "@chakra-ui/react";
-import Google, { GoogleProfile } from "next-auth/providers/google";
-import Nextauth from "../api/auth/[...nextauth]";
-
-// Google handler
-async function handleGoogleSignin(){
-    signIn('google', {callbackUrl: 'https://winnu-git-auth-guissepe.vercel.app'})
-}
-async function handleFacebookSignin(){
-    signIn('facebook', {callbackUrl: 'https://winnu-git-auth-guissepe.vercel.app'})
-}
-async function handleGitHubSignin(){
-    signIn('github', {callbackUrl: 'https://winnu-git-auth-guissepe.vercel.app'})
-}
 
 
-export default function SignIn({ providers }: { providers: any}) {
+export default function SignIn({ providers }: { providers: any }) {
   return (
     <>
-        <Box  background='black' key={providers.name}>
-                    <Button width={250} fontFamily="Sora" variant="primary" py="16px" px="35px" borderRadius="3px" onClick={handleGoogleSignin}>
-                        Sign in with {providers.name}
-                    </Button>
-                    <Button width={250} fontFamily="Sora" variant="primary" py="16px" px="35px" borderRadius="3px" onClick={handleFacebookSignin}>
-                        Sign in with {providers.name}
-                    </Button>
-                    <Button width={250} fontFamily="Sora" variant="primary" py="16px" px="35px" borderRadius="3px" onClick={handleGitHubSignin}>
-                        Sign in with {providers.name}
-                    </Button>
-            {Object.values(providers).map ((provider: any) => (
-                <>
-                <Flex>
-                    {/* <Button width={250} fontFamily="Sora" variant="primary" py="16px" px="35px" borderRadius="3px" onClick={() => signIn(providers.id, { callbackUrl: 'https://winnu-git-auth-guissepe.vercel.app' })}>
-                        Sign in with {providers.name}
-                    </Button> */}
-
-                </Flex>
-
-                </>
-                    ))}
-        </Box>
+    <Box background='black'>
+      {Object.values(providers).map((provider: any) => (
+        <Flex key={provider.name}>
+          <Image src="https://raw.githubusercontent.com/nextauthjs/next-auth/main/packages/next-auth/provider-logos/github.svg"></Image>
+          <Button fontFamily="Sora" variant="primary" py="16px" px="35px" borderRadius="3px" onClick={() => signIn(provider.id, { callbackUrl: 'https://winnu-git-auth-guissepe.vercel.app' })}>
+            Sign in with {provider.name}
+          </Button>
+        </Flex>
+      ))}
+    </Box>
     </>
   )
 }
-
-// SignIn.getInitialProps = async(context: any) => {
-//     const {req, res} = context
-//     const session = await getSession({req});
-
-//     if (session && res && session.accessToken)
-// }
 
 export async function getServerSideProps(context: any) {
   const providers = await getProviders()

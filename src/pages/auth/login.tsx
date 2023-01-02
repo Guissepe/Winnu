b344f9a74a87fde67b3c2e23d89408389350d971
natha ngabriel <nathangabrielvf@gmail.com>
2023-01-02 16:34:08 -0300

    .

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -6,34 +6,27 @@ import {
     Image,
   } from "@chakra-ui/react";
 
-
 export default function SignIn({ providers }: { providers: any }) {
-  return (
-    <>
-    <Box background='black'>
-      {Object.values(providers).map((provider: any) => (
-         <Flex key={provider.name}>
-          <Image src="https://raw.githubusercontent.com/nextauthjs/next-auth/main/packages/next-auth/provider-logos/github.svg"></Image>
-          <Button fontFamily="Sora" variant="primary" py="16px" px="35px" borderRadius="3px">
-            Sign in with
-          </Button>
-        </Flex>
-          ))}
-    </Box>
-
-    </>
-  )
-}
-
-export async function getServerProps(context: any) {
     try {
         throw new Error('Something bad happened');
       }
       catch(e) {
         console.log(e);
-      }
+      };
+  return (
+    <>
+    <Box background='black'>
+        <Button fontFamily="Sora" variant="primary" py="16px" px="35px" borderRadius="3px" onClick={() => signIn(providers.id, { callbackUrl: 'https://winnu-git-auth-guissepe.vercel.app' })}>
+            Sign in with {providers.name}
+        </Button>
+    </Box>
+    </>
+  );
+};
+
+export async function getServerSideProps(context: any) {
   const providers = await getProviders()
   return {
     props: { providers },
-  }
-}
+  };
+};

693835c9583a85c109bc4983be3634ba2e0baf8c
natha ngabriel <nathangabrielvf@gmail.com>
2023-01-02 12:34:26 -0300

    rebuilding login page

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -10,23 +10,7 @@ import {
 export default function SignIn({ providers }: { providers: any }) {
   return (
     <>
-    <Box background='black'>
-      {Object.values(providers).map((provider: any) => (
-        <Flex key={provider.name}>
-          <Image src="https://raw.githubusercontent.com/nextauthjs/next-auth/main/packages/next-auth/provider-logos/github.svg"></Image>
-          <Button fontFamily="Sora" variant="primary" py="16px" px="35px" borderRadius="3px" onClick={() => signIn(provider.id, { callbackUrl: 'https://winnu-git-auth-guissepe.vercel.app' })}>
-            Sign in with {provider.name}
-          </Button>
-        </Flex>
-      ))}
-    </Box>
+
     </>
   )
-}
-
-export async function getServerSideProps(context: any) {
-  const providers = await getProviders()
-  return {
-    props: { providers },
-  }
 }

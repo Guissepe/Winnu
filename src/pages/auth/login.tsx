f5435f7de03d6e1b5aba9968bc294921b4330655
natha ngabriel <nathangabrielvf@gmail.com>
2023-01-02 12:44:45 -0300

    Checking if erro is solved

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -11,22 +11,22 @@ export default function SignIn({ providers }: { providers: any }) {
   return (
     <>
     <Box background='black'>
-
-        <Flex key={providers.name}>
+      {Object.values(providers).map((provider: any) => (
+        <Flex key={provider.name}>
+          <Image src="https://raw.githubusercontent.com/nextauthjs/next-auth/main/packages/next-auth/provider-logos/github.svg"></Image>
+          <Button fontFamily="Sora" variant="primary" py="16px" px="35px" borderRadius="3px" onClick={() => signIn(provider.id, { callbackUrl: 'https://winnu-git-auth-guissepe.vercel.app' })}>
+            Sign in with {provider.name}
+          </Button>
         </Flex>
-
+      ))}
     </Box>
-
     </>
   )
 }
 
 export async function getServerSideProps(context: any) {
-    const providers = await getProviders()
-    return {
-      props: { providers },
-    }
+  const providers = await getProviders()
+  return {
+    props: { providers },
   }
-
-
-
+}

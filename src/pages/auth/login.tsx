2584acd288aa506a8aa93150f95e1fa77a3b149a
natha ngabriel <nathangabrielvf@gmail.com>
2023-01-02 11:39:28 -0300

    Reversed login button changes

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -9,22 +9,22 @@ import Google, { GoogleProfile } from "next-auth/providers/google";
 import Nextauth from "../api/auth/[...nextauth]";
 
 // Google handler
-async function handleGoogleSignin(){
-    signIn('google', {callbackUrl: 'https://winnu-git-auth-guissepe.vercel.app'})
-}
-async function handleFacebookSignin(){
-    signIn('facebook', {callbackUrl: 'https://winnu-git-auth-guissepe.vercel.app'})
-}
-async function handleGitHubSignin(){
-    signIn('github', {callbackUrl: 'https://winnu-git-auth-guissepe.vercel.app'})
-}
+// async function handleGoogleSignin(){
+//     signIn('google', {callbackUrl: 'https://winnu-git-auth-guissepe.vercel.app'})
+// }
+// async function handleFacebookSignin(){
+//     signIn('facebook', {callbackUrl: 'https://winnu-git-auth-guissepe.vercel.app'})
+// }
+// async function handleGitHubSignin(){
+//     signIn('github', {callbackUrl: 'https://winnu-git-auth-guissepe.vercel.app'})
+// }
 
 
 export default function SignIn({ providers }: { providers: any}) {
   return (
     <>
         <Box  background='black' key={providers.name}>
-                    <Button width={250} fontFamily="Sora" variant="primary" py="16px" px="35px" borderRadius="3px" onClick={handleGoogleSignin}>
+                    {/* <Button width={250} fontFamily="Sora" variant="primary" py="16px" px="35px" borderRadius="3px" onClick={handleGoogleSignin}>
                         Sign in with {providers.name}
                     </Button>
                     <Button width={250} fontFamily="Sora" variant="primary" py="16px" px="35px" borderRadius="3px" onClick={handleFacebookSignin}>
@@ -32,13 +32,13 @@ export default function SignIn({ providers }: { providers: any}) {
                     </Button>
                     <Button width={250} fontFamily="Sora" variant="primary" py="16px" px="35px" borderRadius="3px" onClick={handleGitHubSignin}>
                         Sign in with {providers.name}
-                    </Button>
+                    </Button> */}
             {Object.values(providers).map ((provider: any) => (
                 <>
                 <Flex>
-                    {/* <Button width={250} fontFamily="Sora" variant="primary" py="16px" px="35px" borderRadius="3px" onClick={() => signIn(providers.id, { callbackUrl: 'https://winnu-git-auth-guissepe.vercel.app' })}>
+                    <Button width={250} fontFamily="Sora" variant="primary" py="16px" px="35px" borderRadius="3px" onClick={() => signIn(providers.id, { callbackUrl: 'https://winnu-git-auth-guissepe.vercel.app' })}>
                         Sign in with {providers.name}
-                    </Button> */}
+                    </Button>
 
                 </Flex>
 

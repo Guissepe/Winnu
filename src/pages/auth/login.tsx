7ac3733f0bbe325322f7af9c4fb6fc7adb9acdf8
natha ngabriel <nathangabrielvf@gmail.com>
2023-01-03 15:01:51 -0300

    Testing buttons

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -6,17 +6,47 @@ import {
     Image,
   } from "@chakra-ui/react";
 
+  import Google, { GoogleProfile } from "next-auth/providers/google";
+  import Nextauth from "../api/auth/[...nextauth]";
+
+
+  async function handleGoogleSignin(){
+    signIn('google', {callbackUrl: 'https://winnu-git-auth-guissepe.vercel.app/user'})
+}
+async function handleFacebookSignin(){
+    signIn('facebook', {callbackUrl: 'https://winnu-git-auth-guissepe.vercel.app/user'})
+}
+async function handleGitHubSignin(){
+    signIn('github', {callbackUrl: 'https://winnu-git-auth-guissepe.vercel.app/user'})
+}
+
+
 export default function SignIn({ providers }: { providers: any }) {
+
+
+// Google handler
+
   return (
     <>
     <Box background='black'>
-      {Object.values(providers).map((provider: any) => (
-         <Flex key={provider.name}>
+            <Button width={250} fontFamily="Sora" variant="primary" py="16px" px="35px" borderRadius="3px" onClick={ () => handleGoogleSignin()}>
+                Sign in with {providers.name}
+            </Button>
+            <Button width={250} fontFamily="Sora" variant="primary" py="16px" px="35px" borderRadius="3px" onClick={ () => handleFacebookSignin()}>
+                Sign in with {providers.name}
+            </Button>
+            <Button width={250} fontFamily="Sora" variant="primary" py="16px" px="35px" borderRadius="3px" onClick={ () => handleGitHubSignin()}>
+                Sign in with {providers.name}
+            </Button>
+    {/* {Object.values(providers).map((provider: any) => (
+        <Flex key={provider.name}>
+
+
             <Button fontFamily="Sora" variant="primary" py="16px" px="35px" borderRadius="3px" onClick={() => signIn()}>
                 Sign in with {provider.name}
             </Button>
         </Flex>
-          ))}
+          ))} */}
     </Box>
     </>
   );

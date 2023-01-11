aba7b284a869d0cd69b9ef4cddc8e73f1ed57032
natha ngabriel <nathangabrielvf@gmail.com>
2023-01-11 16:47:42 -0300

    Wrong import

--- src/pages/slice-simulator.tsx
+++ src/pages/slice-simulator.tsx
@@ -1,6 +1,6 @@
 import { SliceSimulator } from "@prismicio/slice-simulator-react";
 import { SliceZone } from "@prismicio/react";
-import { components } from "../slices";
+import { components } from "../../slices";
 import state from "../.slicemachine/libraries-state.json";
 
 const SliceSimulatorPage = () => {

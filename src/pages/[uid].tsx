fb3d9a4dc68b7ec58a0ff4a52d7cfba68f0a9698
natha ngabriel <nathangabrielvf@gmail.com>
2023-01-18 09:41:39 -0300

    a

--- src/pages/[uid].tsx
+++ src/pages/[uid].tsx
@@ -1,38 +1,38 @@
 import * as prismicH from '@prismicio/helpers'
-import { SliceZone } from '@prismicio/react'
-import { createClient} from '../../prismicio'
-import { components } from '../../slices/index'
-import { Box } from '@chakra-ui/react'
+// import { SliceZone } from '@prismicio/react'
+// import { createClient} from '../../prismicio'
+// import { components } from '../../slices/index'
+// import { Box } from '@chakra-ui/react'
 
-type PageParams = { uid: string }
-export default async function Page({ page, navigation }: any) {
-  return (
-    <Box>
-      <SliceZone slices={page.data.slices} components={components} />
-    </Box>
-  )
-}
+// type PageParams = { uid: string }
+// export default async function Page({ page, navigation }: any) {
+//   return (
+//     <Box>
+//       <SliceZone slices={page.data.slices} components={components} />
+//     </Box>
+//   )
+// }
 
-export async function getStaticProps({ params, previewData }: any) {
-  const client = createClient({ previewData })
-  const [navigation, page] = await Promise.all([
-   client.getByUID('page', params.uid),
-   client.getByUID('navigation', 'header'),
-  ])
+// export async function getStaticProps({ params, previewData }: any) {
+//   const client = createClient({ previewData })
+//   const [navigation, page] = await Promise.all([
+//    client.getByUID('page', params.uid),
+//    client.getByUID('navigation', 'header'),
+//   ])
 
-  return {
-    props: {
-      navigation,
-      page,
-    },
-  }
-}
+//   return {
+//     props: {
+//       navigation,
+//       page,
+//     },
+//   }
+// }
 
-export async function getStaticPaths() {
-  const client = createClient()
-  const pages = await client.getAllByType('page')
-  return {
-    paths: pages.map(page => prismicH.asLink(page)),
-    fallback: false,
-  }
-}
+// export async function getStaticPaths() {
+//   const client = createClient()
+//   const pages = await client.getAllByType('page')
+//   return {
+//     paths: pages.map(page => prismicH.asLink(page)),
+//     fallback: false,
+//   }
+// }

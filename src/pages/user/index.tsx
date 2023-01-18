fe6de225eb8f602ab23b1e6db31cf084a78a3dac
natha ngabriel <nathangabrielvf@gmail.com>
2023-01-18 12:31:00 -0300

    e

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -21,88 +21,81 @@ const about = () => {
             </Head>
         {session &&
         <Center>
-            <Box
-                textAlign="center"
-                fontFamily="sora"
-                pt={{ base: "47px", md: "94px" }}
-                pb={{ base: "26px", md: "106px" }}
-            >
-
-                {
-                session &&
-                <>
-
-                    <Image src={session.user.picture}/>
-                    <Text
-                    color='white' >
-                        {session.user.name ?? session.user.email}!
-                    </Text>
-                <Text mb='10px'> </Text> <br />
-                </>
-                }
-
-            <Box ml='22%'>
-                <Box >
-
-                    <Text pt='20px' pb='5px' mr='88%' color='#787878'>
-                    Username
-                    </Text>
-                    <InputField/>
-
-                    <Text pt='20px' pb='5px' mr='85%' color='#787878'>
-                    Display name
-                    </Text>
-                    <InputField/>
-
-                </Box>
-
-                <Box mt='20px'>
-                    <Text
-                    color='#787878'
-                    mr='79%'
-                    pb='5px'
-                    >
-                        Describe your role
-                    </Text>
-                    <Dropdown
-                        text1 ='Designer'
-                        text2='Front-End Developer'
-                        text3='Back-End Developer'
-                        text4='Developer'
-                        text5='Freelancer'
-                        text6='Project Manager'
-                        text7='Product Manager'
-                        text8='Student'
-                        text9='Other'
-                    />
-                </Box>
-                <Box mb='50px' mt='20px'>
-                    <Text
-                    color='#787878'
-                    mr='65%'
-                    pb='5px'
-                    >
-                        How do you want to use Winnu
-                    </Text>
-                    <Dropdown
-                        text1 ='Institutional'
-                        text2='Personal'
-                        text3='Student'
-                        text4='Other'
-                    />
-                </Box>
+          <Box
+            textAlign="center"
+            fontFamily="sora"
+            pt={{ base: "47px", md: "94px" }}
+            pb={{ base: "26px", md: "106px" }}
+          >
+            {
+            session &&
+            <>
+                <Image src={session.user.picture}/>
+                <Text
+                color='white' >
+                    {session.user.name ?? session.user.email}!
+                </Text>
+            <Text mb='10px'> </Text> <br />
+            </>
+            }
+            <Box>
+              <Box >
+                <Text pt='20px' pb='5px' mr='88%' color='#787878'>
+                  Username
+                </Text>
+                <InputField/>
+                <Text pt='20px' pb='5px' mr='85%' color='#787878'>
+                  Display name
+                </Text>
+                <InputField/>
+              </Box>
+              <Box mt='20px'>
+                <Text
+                color='#787878'
+                mr='79%'
+                pb='5px'
+                >
+                  Describe your role
+                </Text>
+                <Dropdown
+                  text1 ='Designer'
+                  text2='Front-End Developer'
+                  text3='Back-End Developer'
+                  text4='Developer'
+                  text5='Freelancer'
+                  text6='Project Manager'
+                  text7='Product Manager'
+                  text8='Student'
+                  text9='Other'
+                />
+              </Box>
+              <Box mb='50px' mt='20px'>
+                <Text
+                  color='#787878'
+                  mr='65%'
+                  pb='5px'
+                >
+                  How do you want to use Winnu
+                </Text>
+                <Dropdown
+                  text1 ='Institutional'
+                  text2='Personal'
+                  text3='Student'
+                  text4='Other'
+                />
+              </Box>
             </Box>
             <Box >
-                    <Button
-                    width='423px'
-                    height='65px'
-                    background='#EBC142'
-                    >
-                        <Text >
-                            Create an account
-                        </Text>
-                    </Button>
-                </Box>
+            <Button
+            width='423px'
+            height='65px'
+            background='#EBC142'
+            >
+            <Text >
+                Create an account
+            </Text>
+          </Button>
+        </Box>
         </Box>
         </Center>},
         {!session &&

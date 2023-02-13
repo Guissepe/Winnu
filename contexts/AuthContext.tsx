92ad1b72d75f4a2e59d7fba05b0c3b0d3562b1d5
natha ngabriel <nathangabrielvf@gmail.com>
2023-02-13 09:11:01 -0300

    EE

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,112 +1,112 @@
-import Router from "next/dist/client/router";
+// import Router from "next/dist/client/router";
 import { createContext, ReactNode, useEffect, useState } from "react";
 
-type User = {
-  email: string;
-  permissions: string[];
-  roles: string[];
-}
+// type User = {
+//   email: string;
+//   permissions: string[];
+//   roles: string[];
+// }
 
-type SignInCredentials = {
-  email: string;
-  password: string;
-}
+// type SignInCredentials = {
+//   email: string;
+//   password: string;
+// }
 
-type AuthContextData = {
-  signIn: (credentials: SignInCredentials) => Promise<void>;
-  signOut: () => void;
-  user: User | undefined;
-  isAuthenticated: boolean;
-}
+// type AuthContextData = {
+//   signIn: (credentials: SignInCredentials) => Promise<void>;
+//   signOut: () => void;
+//   user: User | undefined;
+//   isAuthenticated: boolean;
+// }
 
-type AuthProviderProps = {
-  children: ReactNode;
-}
+// type AuthProviderProps = {
+//   children: ReactNode;
+// }
 
-export const AuthContext = createContext({} as AuthContextData);
+// export const AuthContext = createContext({} as AuthContextData);
 
-let authChannel: BroadcastChannel;
+// let authChannel: BroadcastChannel;
 
-export function signOut() {
-  destroyCookie(undefined, 'nextauth.token');
-  destroyCookie(undefined, 'nextauth.refreshToken');
+// export function signOut() {
+//   destroyCookie(undefined, 'nextauth.token');
+//   destroyCookie(undefined, 'nextauth.refreshToken');
 
-  authChannel.postMessage('signOut');
+//   authChannel.postMessage('signOut');
 
-  Router.push('/');
-}
+//   Router.push('/');
+// }
 
-export function AuthProvider({ children }: AuthProviderProps) {
-  const [user, setUser] = useState<User>();
-  const isAuthenticated = !!user;
+// export function AuthProvider({ children }: AuthProviderProps) {
+//   const [user, setUser] = useState<User>();
+//   const isAuthenticated = !!user;
 
-  useEffect(() => {
-    authChannel = new BroadcastChannel('auth');
+//   useEffect(() => {
+//     authChannel = new BroadcastChannel('auth');
 
-    authChannel.onmessage = (message) => {
-      switch (message.data) {
-        case 'signOut':
-          signOut();
-          break;
-        default:
-          break;
-      }
-    }
-  }, []);
+//     authChannel.onmessage = (message) => {
+//       switch (message.data) {
+//         case 'signOut':
+//           signOut();
+//           break;
+//         default:
+//           break;
+//       }
+//     }
+//   }, []);
 
-  useEffect(() => {
-    const { 'nextauth.token': token } = parseCookies();
+//   useEffect(() => {
+//     const { 'nextauth.token': token } = parseCookies();
 
-    if (token) {
-      api.get('/me').then(response => {
-        const { email, permissions, roles } = response.data;
+//     if (token) {
+//       api.get('/me').then(response => {
+//         const { email, permissions, roles } = response.data;
 
-        setUser({ email, permissions, roles });
-      }).catch(() => {
-        signOut();
-      });
-    }
-  }, [])
+//         setUser({ email, permissions, roles });
+//       }).catch(() => {
+//         signOut();
+//       });
+//     }
+//   }, [])
 
-  async function signIn({ email, password }: SignInCredentials) {
+//   async function signIn({ email, password }: SignInCredentials) {
 
-    try {
-      const response = await api.post('sessions', {
-        email,
-        password,
-      });
+//     try {
+//       const response = await api.post('sessions', {
+//         email,
+//         password,
+//       });
 
-      const { token, refreshToken, permissions, roles } = response.data;
+//       const { token, refreshToken, permissions, roles } = response.data;
 
-      setCookie(undefined, 'nextauth.token', token, {
-        maxAge: 60 * 60 * 24 * 30,
-        path: '/',
-      });
+//       setCookie(undefined, 'nextauth.token', token, {
+//         maxAge: 60 * 60 * 24 * 30,
+//         path: '/',
+//       });
 
-      setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
-        maxAge: 60 * 60 * 24 * 30,
-        path: '/',
-      });
+//       setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+//         maxAge: 60 * 60 * 24 * 30,
+//         path: '/',
+//       });
 
-      setUser({
-        email,
-        permissions,
-        roles,
-      });
+//       setUser({
+//         email,
+//         permissions,
+//         roles,
+//       });
 
-      api.defaults.headers['Authorization'] = `Bearer ${token}`;
+//       api.defaults.headers['Authorization'] = `Bearer ${token}`;
 
-      Router.push('/dashboard');
+//       Router.push('/dashboard');
 
-      authChannel.postMessage('signIn');
-    } catch (err) {
-      console.log(err);
-    }
-  }
+//       authChannel.postMessage('signIn');
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   }
 
-  return (
-    <AuthContext.Provider value={{ signIn, signOut, user, isAuthenticated }}>
-      {children}
-    </AuthContext.Provider>
-  )
-}
+//   return (
+//     <AuthContext.Provider value={{ signIn, signOut, user, isAuthenticated }}>
+//       {children}
+//     </AuthContext.Provider>
+//   )
+// }

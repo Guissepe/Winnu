bd5a5f1ed354c90d8f674316b51e8573714a4dd0
natha ngabriel <nathangabrielvf@gmail.com>
2023-02-22 01:48:06 -0300

    e

--- slices/Topimage/index.js
+++ slices/Topimage/index.js
@@ -3,9 +3,9 @@ import { PrismicRichText } from '@prismicio/react';
 import { Center, HStack, Box, Image } from '@chakra-ui/react';
 
 const Topimage = ({ slice }) => (
-  <Center>
+  <Center background={'black'}>
     <Image
-      mb={'2rem'}
+      mb={'5rem'}
       mt={'10rem'}
       height={'2rem'}
       src={slice.primary.image.url}

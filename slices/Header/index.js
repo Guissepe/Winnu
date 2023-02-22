96f5a193eb9abd0fab8c4b3e68668a70d9f02738
natha ngabriel <nathangabrielvf@gmail.com>
2023-02-22 00:16:22 -0300

    we

--- slices/Header/index.js
+++ slices/Header/index.js
@@ -1,43 +1,53 @@
 import React, { useContext } from 'react';
 import { PrismicLink } from '@prismicio/react';
 import { Flex, Image, Text, Button, Center, HStack } from '@chakra-ui/react';
+import {
+  useSession,
+  getSession,
+  getProviders,
+  getCsrfToken,
+} from 'next-auth/react';
 
-const Header = ({ slice }) => (
-  <Center py={'1rem'} background={'black'}>
-    <Flex>
-      <Center>
-        <Image
-          pr={'24rem'}
-          height={'1.25rem'}
-          src={slice.primary.logo.url}
-          alt={slice.primary.logo.alt}
-        />
+const Header = ({ slice }) => {
+  const { data: session, status }: any = useSession();
+  const loading = status === 'loading';
+  return (
+    <Center py={'1rem'} background={'black'}>
+      <Flex>
+        <Center>
+          <Image
+            pr={'24rem'}
+            height={'1.25rem'}
+            src={slice.primary.logo.url}
+            alt={slice.primary.logo.alt}
+          />
+        </Center>
+        <Center mx={'11rem'}>
+          {slice?.items?.map((item, i) => (
+            <HStack key={i}>
+              <PrismicLink field={item.buttonlink}>
+                <Button background={'black'} color={'white'}>
+                  <Text>{item.buttonname}</Text>
+                </Button>
+              </PrismicLink>
+            </HStack>
+          ))}
+        </Center>
+      </Flex>
+      <Center ml={'16rem'}>
+        <PrismicLink field={slice.primary.loglink}>
+          <Button borderRadius={1} background={'black'} color={'white'}>
+            <Text>{slice.primary.login}</Text>
+          </Button>
+        </PrismicLink>
+        <PrismicLink field={slice.primary.startedlink}>
+          <Button borderRadius={1} variant={'primary'} color={'Black'}>
+            <Text>{slice.primary.getstarted}</Text>
+          </Button>
+        </PrismicLink>
       </Center>
-      <Center mx={'11rem'}>
-        {slice?.items?.map((item, i) => (
-          <HStack key={i}>
-            <PrismicLink field={item.buttonlink}>
-              <Button background={'black'} color={'white'}>
-                <Text>{item.buttonname}</Text>
-              </Button>
-            </PrismicLink>
-          </HStack>
-        ))}
-      </Center>
-    </Flex>
-    <Center ml={'16rem'}>
-      <PrismicLink field={slice.primary.loglink}>
-        <Button borderRadius={1} background={'black'} color={'white'}>
-          <Text>{slice.primary.login}</Text>
-        </Button>
-      </PrismicLink>
-      <PrismicLink field={slice.primary.startedlink}>
-        <Button borderRadius={1} variant={'primary'} color={'Black'}>
-          <Text>{slice.primary.getstarted}</Text>
-        </Button>
-      </PrismicLink>
     </Center>
-  </Center>
-);
+  );
+};
 
 export default Header;

a99f16c2b45c598a41bb5e1087bcff48b5e0ef0b
natha ngabriel <nathangabrielvf@gmail.com>
2023-02-21 21:44:42 -0300

    EEEE

--- src/components/Handlers.tsx
+++ src/components/Handlers.tsx
@@ -2,16 +2,16 @@ import { getProviders, signIn } from 'next-auth/react';
 
 export function handleGoogleSignin() {
   signIn('google', {
-    callbackUrl: 'https://winnu-git-auth-guissepe.vercel.app/user',
+    callbackUrl: 'https://winnu-git-auth-guissepe.vercel.app/userpage',
   });
 }
 export function handleFacebookSignin() {
   signIn('facebook', {
-    callbackUrl: 'https://winnu-git-auth-guissepe.vercel.app/user',
+    callbackUrl: 'https://winnu-git-auth-guissepe.vercel.app/userpage',
   });
 }
 export function handleGitHubSignin() {
   signIn('github', {
-    callbackUrl: 'https://winnu-git-auth-guissepe.vercel.app/user',
+    callbackUrl: 'https://winnu-git-auth-guissepe.vercel.app/userpage',
   });
 }
